687ba575bc407803314292a0759d6f1e834ab7af
YashMengji <yashmengji2172005@gmail.com>
2024-12-11 14:27:03 +0530

    designed the calender component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
+import Events from "./components/Events"
 import React from "react"
 
 export default function App() {
@@ -7,12 +8,15 @@ export default function App() {
   const [date, setDate] = React.useState(new Date())
 
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border"
-    />
+    <div className="main-div flex justify-center items-center min-h-screen">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="flex w-max h-max border border-8 rounded-md"
+      />
+      <Events/>
+    </div>
   )
 }
 
