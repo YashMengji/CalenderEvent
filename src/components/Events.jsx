a08915db3304ced9115cf275918b7cceb91ff210
YashMengji <yashmengji2172005@gmail.com>
2024-12-12 11:10:40 +0530

    event properties completed

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 function Events({ ...props }) {
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
   const [eventName, setEventName] = useState('');
   const [eventDesc, setEventDesc] = useState('');
-  const [events, setEvents] = useState([]);
+  const [eventCategory, setEventCategory] = useState('Others');
+  const [events, setEvents] = useState([{id: 1, name: 'sadfdf', desc: '', startTime: '11:01', endTime: '11:01'}]);
 
   useEffect(() => {
     const date = new Date();
@@ -20,6 +29,10 @@ function Events({ ...props }) {
     setStartTime(startTime);
     setEndTime(startTime);
   }, []);
+
+  useEffect(() => {
+    console.log(events)
+  }, [events])
 
   function createEvent(e) {
     e.preventDefault();
@@ -42,12 +55,14 @@ function Events({ ...props }) {
       desc: eventDesc,
       startTime: startTime,
       endTime: endTime,
+      category: eventCategory,
       date: props.date,
     };
     setEvents([...events, newEvent]);
 
     setEventName('');
     setEventDesc('');
+    setEventCategory('');
 
     toast.success('Event created successfully!', {
       position: 'top-right',
@@ -63,7 +78,7 @@ function Events({ ...props }) {
   return (
     <div className="w-[600px] h-[500px] border-black border-2 rounded-xl p-4">
       <form>
-        <div className="div-create-event-input flex flex-col gap-5 w-full rounded-xl">
+        <div className="div-create-event-input flex flex-col gap-4 w-full rounded-xl">
           <div className="div-event-name-input flex flex-col relative w-full">
             <span className="absolute text-red-500 top-[-2px] left-[-8px] text-xl">*</span>
             <input
@@ -81,14 +96,14 @@ function Events({ ...props }) {
             <input
               type="text"
               className="event-desc-input w-3/4 bg-transparent border-b border-black rounded-tl-xl outline-none text-md placeholder-gray-700"
-              placeholder="Event Description (max 100 characters)" 
+              placeholder="Event Description (max 100 characters)"
               value={eventDesc}
               maxLength="100"
               onChange={(e) => setEventDesc(e.target.value)}
             />
           </div>
 
-          <div className="div-event-time flex items-end gap-5">
+          <div className="div-event-time flex items-end gap-6">
             <div className="flex gap-2 relative">
               <span className="absolute text-red-500 top-[-3px] left-[-8px] text-xl">*</span>
               <div className="div-from-label">From:</div>
@@ -126,6 +141,24 @@ function Events({ ...props }) {
             </div>
           </div>
 
+          <div className="flex items-center gap-4 ">
+            <div className="">
+              Label:
+            </div>
+            <div className="border border-black rounded-md " >
+              <Select className="outline-none border-none" value={eventCategory} onValueChange={(value) => setEventCategory(value)}>
+                <SelectTrigger className="w-[180px] " > 
+                  <SelectValue placeholder="Event category" />
+                </SelectTrigger>
+                <SelectContent >
+                  <SelectItem className="bg-blue-500 text-white" value="Work">Work</SelectItem>
+                  <SelectItem className="bg-green-500 text-white" value="Personal">Personal</SelectItem>
+                  <SelectItem value="Others">Others</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
           <div>
             <button
               type="submit"
@@ -138,18 +171,29 @@ function Events({ ...props }) {
           </div>
         </div>
       </form>
-      <div className="div-events-list flex flex-col gap-3 w-full h-[277px] overflow-auto mt-3">
-          {events.map((event) => (
-            <div className="event-card flex flex-col justify-between w-full min-h-[100px] bg-white rounded-xl p-2" key={event.id}>
-              <div className="event-name font-[Montserrat] text-2xl font-extrabold">{event.name}</div>
-              <div className="event-desc break-normal break-words break-all ">{event.desc}</div>
-              <div className="event-time text-zinc-500">
-                {console.log(Math.floor(Number(event.endTime.substring(3)) / 12))}
-                {Number(event.startTime.substring(0, 2)) % 12 === 0 ? "12" : Number(event.startTime.substring(0, 2) % 12).padStart(2, '0') }:{event.startTime.substring(3)} {Math.floor(Number(event.startTime.substring(0, 2)) / 12) === 0 ? "am" : "pm"} - 
-                {Number(event.endTime.substring(0, 2)) % 12 === 0 ? "12" : String(Number(event.endTime.substring(0, 2)) % 12).padStart(2, '0')}:{event.endTime.substring(3)} {Math.floor(Number(event.endTime.substring(0, 2)) / 12) === 0 ? "am" : "pm"}
+
+      <div className="div-events-list flex flex-col gap-3 w-full h-[230px] overflow-auto mt-3">
+        {events.map((event) => (
+          <div className={`event-card flex flex-col justify-between w-full min-h-[100px] bg-white rounded-xl p-2 ${event.category === "Work" ? "bg-blue-500" : event.category === "Personal" ? "bg-green-500" : "bg-zinc-500"}`} key={event.id}>
+            <div className="flex justify-between">
+              <div className="event-name font-[Montserrat] text-2xl text-white font-extrabold">{event.name}</div>
+              <div className="flex items-start gap-3 bg-blue-500 ">
+                <div className="">
+                  <FontAwesomeIcon icon={faPenToSquare} className='text-xl text-white' />
+                </div>
+                <div className="">
+                  <FontAwesomeIcon icon={faTrash} className='text-xl text-red-500' />
+                </div>
               </div>
             </div>
-          ))}
+            <div className="event-desc text-white break-normal break-words break-all ">{event.desc}</div>
+            <div className="event-time text-zinc-300">
+              {console.log(Math.floor(Number(event.endTime.substring(3)) / 12))}
+              {Number(event.startTime.substring(0, 2)) % 12 === 0 ? "12" : String(Number(event.startTime.substring(0, 2)) % 12).padStart(2, '0')}:{event.startTime.substring(3)} {Math.floor(Number(event.startTime.substring(0, 2)) / 12) === 0 ? "am" : "pm"} -
+              {Number(event.endTime.substring(0, 2)) % 12 === 0 ? "12" : String(Number(event.endTime.substring(0, 2)) % 12).padStart(2, '0')}:{event.endTime.substring(3)} {Math.floor(Number(event.endTime.substring(0, 2)) / 12) === 0 ? "am" : "pm"}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
